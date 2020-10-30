b735d77eb13383672d865ee2ef332fdc61dca8cf
Luis Daniel Assulfi <lassulfi@gmail.com>
2020-10-29 21:04:25 -0300

    Definicao dos casos de testes

---
+++ avaliacao/src/main/java/br/utfpr/avaliacao/Triangulo.java
@@ -0,0 +1,19 @@
+package br.utfpr.avaliacao;
+
+public class Triangulo {
+
+    /**
+     * Dado três lados de triangulo, realiza a sua classificação com relação aos seus lados
+     *
+     * @param lado1 {int} lado 1 do triangulo
+     * @param lado2 {int} lado 2 do triangulo
+     * @param lado3 {int} lado 3 do triangulo
+     * @return {String} "equilatero" se o triangulo possui três lados iguais
+     *                  "isoceles" se o triangulo possui dois lados iguais
+     *                  "escaleno" se o triangulo possui três lados diferentes
+     */
+    public String classificar(int lado1, int lado2, int lado3) {
+
+        return null;
+    }
+}
